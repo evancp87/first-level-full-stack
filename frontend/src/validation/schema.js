160d91c4526249bd5f43e3bd3c096906da908793
Evan Parker <65721884+evancp87@users.noreply.github.com>
2023-08-04 09:27:07 +0100

    added joi validation for authentication components

--- frontend/src/validation/schema.js
+++ frontend/src/validation/schema.js
@@ -3,12 +3,19 @@ export const searchSchema = {
   search: joi.string().trim().min(2).required().label("Search"),
 };
 
-// export const addUser = {
-//   username: joi.string().trim(),
+// export const login = {
 //   firstName: joi.string().trim().min(2).required.pattern().lowercase().messages(),
 //   lastName: joi.string().trim().min(2).required,
 //   email: joi.string().trim().min(2).required,
 //   password: joi.string().trim().min(2).required,
 //   address: joi.string().trim().min(2).required,
 
+// }
+
+// export const register = {
+//   firstName: joi.string().trim().min(2).required.pattern().lowercase().messages(),
+//   lastName: joi.string().trim().min(2).required,
+//   email: joi.string().trim().min(2).required,
+//   password: joi.string().trim().min(2).required,
+//   address: joi.string().trim().min(2).required,
 // }
