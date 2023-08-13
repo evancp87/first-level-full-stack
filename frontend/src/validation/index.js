334287877cda19fb5e524bb72d51a0d623edcc26
Evan Parker <65721884+evancp87@users.noreply.github.com>
2023-08-13 08:35:10 +0100

    feat: implemented validation for user authentication

--- frontend/src/validation/index.js
+++ frontend/src/validation/index.js
@@ -1,8 +1,56 @@
 import joi from "joi";
-import { searchSchema } from "./schema";
+import { searchSchema, loginSchema, registerSchema } from "./schema";
 
 export const validate = async (payload) => {
   const searchBox = joi.object(searchSchema);
+
+  try {
+    const results = await searchBox.validateAsync(payload, {
+      abortEarly: false,
+    });
+
+    console.log(results);
+    return null;
+  } catch (error) {
+    console.log(error);
+    if (error.details) {
+      // configures Joi's slightly unusual data structure to be more useful
+      const errorsMod = error.details.map((error) => ({
+        key: error.context.key,
+        message: error.message,
+      }));
+      return errorsMod;
+    }
+    return [error.message];
+  }
+};
+
+export const validateLogin = async (payload) => {
+  const searchBox = joi.object(loginSchema);
+
+  try {
+    const results = await searchBox.validateAsync(payload, {
+      abortEarly: false,
+    });
+
+    console.log(results);
+    return null;
+  } catch (error) {
+    console.log(error);
+    if (error.details) {
+      // configures Joi's slightly unusual data structure to be more useful
+      const errorsMod = error.details.map((error) => ({
+        key: error.context.key,
+        message: error.message,
+      }));
+      return errorsMod;
+    }
+    return [error.message];
+  }
+};
+
+export const validateRegister = async (payload) => {
+  const searchBox = joi.object(registerSchema);
 
   try {
     const results = await searchBox.validateAsync(payload, {
--- frontend/src/validation/schema.js
+++ frontend/src/validation/schema.js
@@ -1,21 +1,28 @@
 import joi from "joi";
 export const searchSchema = {
-  search: joi.string().trim().min(2).required().label("Search"),
+  search: joi.string().trim().min(2).label("Search"),
 };
 
-// export const login = {
-//   firstName: joi.string().trim().min(2).required.pattern().lowercase().messages(),
-//   lastName: joi.string().trim().min(2).required,
-//   email: joi.string().trim().min(2).required,
-//   password: joi.string().trim().min(2).required,
-//   address: joi.string().trim().min(2).required,
+export const loginSchema = {
+  email: joi.string().trim().min(2),
+  password: joi.string().trim().min(2),
+};
 
-// }
-
-// export const register = {
-//   firstName: joi.string().trim().min(2).required.pattern().lowercase().messages(),
-//   lastName: joi.string().trim().min(2).required,
-//   email: joi.string().trim().min(2).required,
-//   password: joi.string().trim().min(2).required,
-//   address: joi.string().trim().min(2).required,
-// }
+export const registerSchema = {
+  email: joi
+    .string()
+    .trim()
+    .email({ minDomainSegments: 2, tlds: { allow: false } })
+    .label("email"),
+  name: joi.string().trim().min(1).max(40).label("name"),
+  password: joi
+    .string()
+    .trim()
+    .min(1)
+    .max(40)
+    .regex(/[0-9a-zA-Z]*\d[0-9a-zA-Z]*/)
+    .label("password"),
+  // .rule({
+  //   message: "Your password requires at least one number and character",
+  // }),
+};
