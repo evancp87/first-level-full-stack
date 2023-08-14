56b2ceddb81deeb0d3da79e2e13314baae938c02
Evan Parker <65721884+evancp87@users.noreply.github.com>
2023-08-14 23:56:32 +0100

    feat: tweak to max requests in rate limiter

--- backend/middleware/rate-limiter.js
+++ backend/middleware/rate-limiter.js
@@ -1,7 +1,7 @@
 const rateLimiter = require("express-rate-limit");
 
 const limiter = rateLimiter({
-  max: 5,
+  max: 500,
   windowMs: 5000,
   message:
     "you can't make any more requests at the moment, please try again later",
