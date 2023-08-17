7940825534e0f41c859a11669a6a051de25ec5bd
Evan Parker <65721884+evancp87@users.noreply.github.com>
2023-08-17 15:30:28 +0100

    feat: live db details

--- backend/database/connection.js
+++ backend/database/connection.js
@@ -1,11 +1,16 @@
 const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-  user: "root",
-  password: "",
-  host: "localhost",
+  // user: "root",
+  // password: "",
+  // host: "localhost",
+  // port: 3306,
+  // database: "games-db",
+  user: "sql8640488",
+  password: "QetzYuTDvf",
+  host: "sql8.freemysqlhosting.net",
   port: 3306,
-  database: "games-db",
+  database: "sql8640488",
 });
 
 function asyncMySQL(query, params) {
