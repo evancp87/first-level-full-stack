f8a8d30a15b4f9554cb6b1bc47f835fbd78101a4
Evan Parker <65721884+evancp87@users.noreply.github.com>
2023-08-18 08:44:57 +0100

    feat: added handling for x-forwarded-for header

--- backend/middleware/rate-limiter.js
+++ backend/middleware/rate-limiter.js
@@ -3,6 +3,7 @@ const rateLimiter = require("express-rate-limit");
 const limiter = rateLimiter({
   max: 500,
   windowMs: 5000,
+  trustProxy: true,
   message:
     "you can't make any more requests at the moment, please try again later",
 });
